255035688725381b364d205c3d5095e94494083f
Felipe <jfpastrana.uninorte.edu.co>
2021-10-30 01:07:12 -0500

    app agregado funcion para registrar usuarios, action del formulario de registro html /registro_usuario

--- app.py
+++ app.py
@@ -1,46 +1,49 @@
-from flask import Flask, render_template, request 
-import werkzeug.security as ws #libreria para guardar contraseñas
+from flask import Flask, render_template, request
+import werkzeug.security as ws
+from werkzeug.utils import redirect  # libreria para guardar contraseñas
 #import db
 
-app=Flask(__name__)
+app = Flask(__name__)
 app.secret_key = 'mi_clave_secreta'
+
 
 @app.route('/')
 def inicio():
     return render_template('inicio.html')
 
-@app.route('/login',methods=['GET', 'POST'])
+
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    if request.method=='GET':
+    if request.method == 'GET':
         return render_template('login.html')
     else:
-        print(request.form) #validar
+        print(request.form)  # validar
         return render_template('perfil.html')
-    
 
-@app.route('/registro', methods=['GET', 'POST'])
+
+@app.route('/registro')
 def registro():
-    if request.method=='GET':
-        return render_template('registro.html')
-    else:
-        print(request.form) #validar
-        nombre = request.form('nombre')
-        usuario = request.form('usuario')
-        correo = request.form('correo')
-        contrasena = request.form('contrasena')
-        confirmar_contrasena = request.form('confirmar_contrasena')
-        
-        #db.insertar_usuario(nombre, usuario, correo, ws,ws.generate_password_hash(contrasena)) #se debe validar la contraseña al igual que se hizo en registro.html
-        
-        return 'Registro éxitoso'
+    return render_template('registro.html')
+
+@app.route('/registro_usuario', methods=['POST'])
+def registrarUsuario():
+    nombre = request.form['nombre']
+    usuario = request.form['usuario']
+    correo = request.form['correo']
+    contraseña = request.form['contrasena']
+    confirmar_contraseña = request.form['confirmar_contrasena']
+    return redirect('/login')
+
 
 @app.route('/busqueda')
 def buscar():
     return render_template('busqueda.html')
 
+
 @app.route('/menu')
 def menu():
     return render_template('menu.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
