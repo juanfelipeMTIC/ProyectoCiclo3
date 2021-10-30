bd0ea75f839f4d6b6f7f01f8092e1761f0619f97
Felipe <jfpastrana.uninorte.edu.co>
2021-10-30 02:04:29 -0500

    se modifico el css body agregando efectos para los botones, se modificaron las imagenes de los botones de contraseñas, se agrego el boton de ver contraseña al login, se modificaron levemente los login y registro

--- static/js/login.js
+++ static/js/login.js
@@ -4,3 +4,20 @@ btn_registrate.addEventListener('click', () =>{
     window.location.href='/registro'
 })
 
+let img_mostrar_contrasena = document.getElementById('ver_contrasena');
+ver_click(img_mostrar_contrasena, 'contrasena')
+
+function ver_click(boton, id_input) {
+  boton.addEventListener("click", () => {
+    let input_contrasena = document.getElementById(id_input);
+    let tipo_input_contrasena = input_contrasena.getAttribute("type");
+
+    if (tipo_input_contrasena == "password") {
+      input_contrasena.setAttribute("type", "text");
+      boton.setAttribute("src", "/static/img/Close_eye.png");
+    } else {
+      input_contrasena.setAttribute("type", "password");
+      boton.setAttribute("src", "/static/img/Open_eye.png");
+    }
+  });
+}
